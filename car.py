8c25547239a7939058118f125e0cf1f167bb5064
mingyue312 <mingyue312@gmail.com>
2016-05-30 16:52:56 -0400

    monday submit 2

--- car.py
+++ car.py
@@ -1,13 +1,16 @@
 import macros
+import path
 
 
 class Car:
-    def __init__(self, time_tag, index, initial_ent, speed, path, location):
+    def __init__(self, time_tag, index, initial_ent, initial_dir, speed, location):
         self.time_tag = time_tag
         self.index = index
         self.initial_ent = initial_ent
+        self.initial_dir = initial_dir
         self.final_exit = None
-        self.path = path
+        self.final_dir = 0
+        self.my_path = path.find_path(self.initial_ent, self.final_exit)
         self.speed = speed
         self.acc = 0
         self.wait_time = 0
@@ -15,7 +18,5 @@ class Car:
         self.next = None
         self.prev = None
         self.location = location
-
-
 
 
--- path.py
+++ path.py
@@ -1,5 +1,81 @@
 import macros
 import map_init
+import random
 
 
-def find_path()
+def find_path(init, fin):
+    '''
+    initial_ent: the intersection of initial entrance, example: intersection (1,1)
+    final_exit: the intersection of final exit, example: intersection (1,3)
+    '''
+    path = []
+
+    if init == fin:
+        print("Error: cars cannot have same entrance and exit intersection!!!")
+        return -1
+    
+    if init[0] == fin[0]:          # only need to travel horizontally
+        if fin[1] > init[1]:       # move right
+            for i in range(init[1], fin[1]+1):
+                path.append((init[0], i))
+        else:                       # move left
+            for i in range(init[1], fin[1]-1, -1):
+                path.append((init[0], i))
+
+    elif init[1] == fin[1]:        # only need to travel vertically
+        if fin[0] > init[0]:       # move down
+            for i in range(init[0], fin[0]+1):
+                path.append((i, init[1]))
+        else:                      # move up
+            for i in range(init[0], fin[0]-1, -1):
+                path.append((i, init[1]))
+
+    else:                          # need to travel both horizontally and vertically
+        if fin[0] > init[0] and fin[1] > init[1]:   # to right and down
+            if random.randint(0,1):                 # right then down
+                for i in range(init[1], fin[1]+1):
+                    path.append((init[0], i))
+                for i in range(init[0]+1, fin[0]+1):
+                    path.append((i, fin[1]))
+            else:                                   # down then right
+                for i in range(init[0], fin[0]+1):
+                    path.append((i, init[1]))
+                for i in range(init[1]+1, fin[1]+1):
+                    path.append((fin[0], i))
+
+        elif fin[0] < init[0] and fin[1] > init[1]: # to right and up
+            if random.randint(0,1):                 # right then up
+                for i in range(init[1], fin[1]+1):
+                    path.append((init[0], i))
+                for i in range(init[0]-1, fin[0]-1, -1):
+                    path.append((i, fin[1]))
+            else:                                   # up then right
+                for i in range(init[0], fin[0]-1, -1):
+                    path.append((i, init[1]))
+                for i in range(init[1]+1, fin[1]+1):
+                    path.append((fin[0], i))
+
+        elif fin[0] > init[0] and fin[1] < init[1]: # to left and down
+            if random.randint(0,1):                 # left then down
+                for i in range(init[1], fin[1]-1, -1):
+                    path.append((init[0], i))
+                for i in range(init[0]+1, fin[0]+1):
+                    path.append((i, fin[1]))
+            else:                                   # down the left
+                for i in range(init[0], fin[0]+1):
+                    path.append((i, init[1]))
+                for i in range(init[1]-1, fin[1]-1, -1):
+                    path.append((fin[0], i))
+        else:                                       # to left and up
+            if random.randint(0,1):                 # left then up
+                for i in range(init[1], fin[1]-1, -1):
+                    path.append((init[0], i))
+                for i in range(init[0]-1, fin[0]-1, -1):
+                    path.append((i, fin[1]))
+            else:                                   # up then left
+                for i in range(init[0], fin[0]-1, -1):
+                    path.append((i, init[1]))
+                for i in range(init[1]-1, fin[1]-1, -1):
+                    path.append((fin[0], i))
+
+    return path
