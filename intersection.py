a7a86a025fc4082698398c44792203d02fa4ecb9
mingyue312 <mingyue312@gmail.com>
2016-06-01 18:16:48 -0400

    intersection changed

--- intersection.py
+++ intersection.py
@@ -9,14 +9,8 @@ class Intersection:
         self.south_len = south_len
         self.west_len = west_len
         self.east_len = east_len
-        self.car_from_nl = None
-        self.car_from_sl = None
-        self.car_from_wl = None
-        self.car_from_el = None
-        self.car_from_nr = None
-        self.car_from_sr = None
-        self.car_from_wr = None
-        self.car_from_er = None
+        self.cars_queue = {macros.WESTL: None, macros.WESTR: None, macros.EASTL: None, macros.EASTR: None,
+                           macros.NORTHL: None, macros.NORTHR: None, macros.SOUTHL: None, macros.SOUTHR: None}
         self.north = None
         self.south = None
         self.west = None
--- network_control.py
+++ network_control.py
@@ -5,15 +5,34 @@ import macros
 
 map_init.map_init()
 
-sim_time = 0
 
-
-def network_control(duration, time_increment, num_inters):
-    while sim_time <= duration:
+def network_control():
+    while macros.SIM_TIME <= macros.DURATION:
         for inter in map_init.intersections:
+
             for enter_lane in map_init.intersections[inter].boundary:
-                if sim_time >= map_init.intersections[inter].boundary[enter_lane]: #if time to create a new car for intersection 'inter' and lane 'enter_lane'
-                    new_car = car.Car(inter, macros.INITSPEED)
+                if macros.SIM_TIME >= map_init.intersections[inter].boundary[enter_lane]: #if time to create a new car for intersection 'inter' and lane 'enter_lane'
+                    if enter_lane in [macros.WESTL, macros.WESTR]:
+                        direction = macros.WEST
+                    elif enter_lane in [macros.EASTL, macros.EASTR]:
+                        direction = macros.EAST
+                    elif enter_lane in [macros.NORTHL, macros.NORTHR]:
+                        direction = macros.NORTH
+                    else:
+                        direction = macros.SOUTH
+                    new_car = car.Car(inter, direction, macros.INITSPEED)
+                    if not map_init.intersections[inter].cars_queue[enter_lane]:
+                        map_init.intersections[inter].cars_queue[enter_lane] = new_car
+                    else:
+                        current_car = map_init.intersections[inter].cars_queue[enter_lane]
+                        while current_car.next:
+                            current_car = current_car.next
+                        current_car.next = new_car
+
+                    # get next arrival time and save it to each lanes of each intersection
                     prev_arrival = map_init.intersections[inter].boundary[enter_lane]
                     map_init.intersections[inter].boundary[enter_lane] = \
                         round(random.uniform(prev_arrival, prev_arrival + macros.FREQ), 2)
+
+
+        macros.SIM_TIME += macros.TIME_INCREMENT
