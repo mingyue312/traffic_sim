e50291e2c720774d4c163516bd8c963d4ba4b37d
mingyue312 <mingyue312@gmail.com>
2016-08-04 13:53:50 -0400

    phase 2 clustering finished

--- network_control.py
+++ network_control.py
@@ -116,21 +116,21 @@ def network_control():
                 elif map_init.intersections[inter].current_phase in [macros.NSYELLOW_EWRED, macros.NSGREEN_EWRED]:
                     queue_len += [map_init.intersections[inter].timer, 0]
                 input_dict[inter] = queue_len
-                #action[inter] = random.randint(0, 1)
+                action[inter] = random.randint(0, 1)
                 #action[inter] = -1
-            action = Qlearning_Scalable.qlearning(input_dict, macros.clusters,prev_state_global,cur_state_global,prev_action_global,n_global)   # pass in the queue_len dictionary to qlearning and get action dictionary back
+            #action = Qlearning_Scalable.qlearning(input_dict, macros.clusters,prev_state_global,cur_state_global,prev_action_global,n_global)   # pass in the queue_len dictionary to qlearning and get action dictionary back
         #if macros.SIM_TIME % 100 == 0:
         #    visualization.draw_cars()
             #visualization.draw_signal()
             #visualization.log_action_table()
             #visualization.log_q_value()
 
-        #if macros.SIM_TIME == 30:
-        #    qlearning_helper.get_first_coherence_list()
-        #    print(macros.prev_coherence_matrix)
-        #if 35 <= macros.SIM_TIME <= 300 and macros.SIM_TIME % 5 == 0:
-        #    qlearning_helper.get_coherence_list()
-        #    print(macros.prev_coherence_matrix)
+        if macros.SIM_TIME == 30:
+            qlearning_helper.get_first_coherence_list()
+            print(macros.prev_coherence_matrix)
+        if 35 <= macros.SIM_TIME <= 300 and macros.SIM_TIME % 5 == 0:
+            qlearning_helper.get_coherence_list()
+            print(macros.prev_coherence_matrix)
 
         #if macros.SIM_TIME == 100:
         #    visualization.log_action_table()
